26dd378db62cf78013c6cf9f20bb22ec8857f41e
DataDev <104727812+KinexDev@users.noreply.github.com>
2024-03-21 20:10:26 +0000

    Uploading mod template

---
+++ Mod Template/Main.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using POKModManager;
+
+namespace Mod_Template
+{
+    [BepInPlugin("Unknown.ModTemplate", "Mod Template", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            POKManager.RegisterMod(new Mod(), "Template mod", "1.0.0", "This is a template mod");
+        }
+    }
+}
---
+++ Mod Template/Mod.cs
@@ -0,0 +1,17 @@
+﻿using POKModManager;
+
+namespace Mod_Template
+{
+    class Mod : ModClass
+    {
+        public override void OnEnabled()
+        {
+            print("Template mod has been enabled!");
+        }
+
+        public override void OnDisabled()
+        {
+            print("Template mod has been disabled!");
+        }
+    }
+}
